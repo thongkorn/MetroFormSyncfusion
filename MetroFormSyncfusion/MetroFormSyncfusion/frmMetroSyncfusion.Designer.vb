9404bbc8a086872b08d45fe6a5e3d27d01d51ba9
Thongkorn Tubtimkrob <thongkorn@hotmail.com>
2022-07-07 18:33:45 +0700

    Add files via upload

---
+++ MetroFormSyncfusion/MetroFormSyncfusion/frmMetroSyncfusion.Designer.vb
@@ -0,0 +1,49 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMetroSyncfusion
+    '//Inherits System.Windows.Forms.Form
+    Inherits Syncfusion.Windows.Forms.MetroForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim CaptionImage1 As Syncfusion.Windows.Forms.CaptionImage = New Syncfusion.Windows.Forms.CaptionImage()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMetroSyncfusion))
+        Me.SuspendLayout()
+        '
+        'frmMetroSyncfusion
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        CaptionImage1.BackColor = System.Drawing.Color.Transparent
+        CaptionImage1.Image = CType(resources.GetObject("CaptionImage1.Image"), System.Drawing.Image)
+        CaptionImage1.Location = New System.Drawing.Point(5, 8)
+        CaptionImage1.Name = "CaptionImage1"
+        Me.CaptionImages.Add(CaptionImage1)
+        Me.ClientSize = New System.Drawing.Size(510, 340)
+        Me.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "frmMetroSyncfusion"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Metro Form Syncfusion"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+End Class
---
+++ MetroFormSyncfusion/MetroFormSyncfusion/frmMetroSyncfusion.vb
@@ -0,0 +1,42 @@
+﻿#Region "ABOUT"
+' / --------------------------------------------------------------------------------
+' / Developer : Mr.Surapon Yodsanga (Thongkorn Tubtimkrob)
+' / email : thongkorn@hotmail.com
+' / URL: http://www.g2gnet.com (Khon Kaen - Thailand)
+' / Facebook: https://www.facebook.com/g2gnet (For Thailand)
+' / Facebook: https://www.facebook.com/commonindy (Worldwide)
+' / More Info: http://www.g2gnet.com/webboard
+' / 
+' / Purpose: Metro Form with Syncfusion Community.
+' / Microsoft Visual Basic .NET (2010)
+' /
+' / This is open source code under @CopyLeft by Thongkorn Tubtimkrob.
+' / You can modify and/or distribute without to inform the developer.
+' / --------------------------------------------------------------------------------
+#End Region
+
+Public Class frmMetroSyncfusion
+
+    Private Sub frmMetroSyncfusion_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Me.Text = ""
+        Me.BorderThickness = 5
+        Me.CaptionBarColor = Color.SeaGreen
+        Me.BorderColor = Color.SeaGreen
+        Me.CaptionBarHeight = 40
+        Me.ShowIcon = False '/ Form not show icon.
+        Me.DropShadow = True
+        Me.CaptionButtonHoverColor = System.Drawing.Color.Red
+        '//
+        Dim CaptionLabel1 As Syncfusion.Windows.Forms.CaptionLabel = New Syncfusion.Windows.Forms.CaptionLabel
+        With CaptionLabel1
+            CaptionLabel1.Font = New System.Drawing.Font("Tahoma", 10.0F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+            .Text = "Metro Form Syncfusion Community"
+            .ForeColor = System.Drawing.Color.White
+            .Name = "CaptionLabel1"
+            .Size = New System.Drawing.Size(400, 34)
+            .Location = New Point(33, 5)
+        End With
+        '/ Add CaptionLabel with code.
+        Me.CaptionLabels.Add(CaptionLabel1)
+    End Sub
+End Class
